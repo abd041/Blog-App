4cac792d478dc8c99598a9491a77d007501d59e5
Muhammad Abdullah Rauf <abdullahrauf440@gmail.com>
2025-10-08 11:22:22 +0500

    add admin blog sidebar creted

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../../components/adminComponents/Sidebar";
+
+export default function Layout({ children }) {
+    return (
+        <>
+        <div className="flex ">
+            <Sidebar/>
+        </div>
+            {children}
+        </>
+    )
+}
